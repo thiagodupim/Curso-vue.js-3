c77f20bb4a3bc4ffd84be7a8e19ed8d3282dab0b
Thiago Dupim Aguilar <thiagodupim3@gmail.com>
2025-09-29 16:07:17 -0300

    Atributos dinâmicos com data binding (diretiva v-bind :

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <div v-for="(obj, index) in todos" v-bind:key="obj.id" class="todos-item">
+      <img 
+        v-if="obj.imgSrc" 
+          :src="obj.imgSrc"
+      >
       {{ index }} - {{ obj.title }}
     </div>
   </div>
@@ -19,13 +23,15 @@ export default {
           "userId": 1,
           "id": 1,
           "title": "delectus aut autem",
-          "completed": false
+          "completed": false,
+          "imgSrc": 'https://placehold.co/150'
         },
         {
           "userId": 1,
           "id": 2,
           "title": "quis ut nam facilis et officia qui",
-          "completed": false
+          "completed": false,
+          "imgSrc": 'https://placehold.co/150'
         },
         {
           "userId": 1,
