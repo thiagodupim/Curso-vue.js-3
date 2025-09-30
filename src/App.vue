c5bcdea328edcc668b403bfb8d092e58cf5a976f
Thiago Dupim Aguilar <thiagodupim3@gmail.com>
2025-09-30 16:33:17 -0300

    Ligação de dados bidirecional (diretiva v-model)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div>
-    <h1 :class="{ 'title': true, 'title-home': isHome }">
-      Curso VueJS 3
-    </h1>
-
-    <p :class="pClass">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat provident reprehenderit distinctio! Consequuntur, error harum, asperiores eaque perspiciatis unde illum corrupti assumenda, sapiente deserunt atque. Illo aut iusto neque dolore.
-    </p>
-
-    <p :style="styleClass">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem cum exercitationem fuga aperiam alias dolores tenetur quasi voluptatem voluptatibus porro ipsam optio incidunt cupiditate facere, illo esse, ipsum fugit sunt.
-    </p>
-
-    <div 
-      v-for="(obj, index) in todos" 
-      :key="obj.id" 
-      class="todos-item"
-      >
-      <img 
-        v-if="obj.imgSrc" 
-        :src="obj.imgSrc"
-      >
-      {{ index }} - {{ obj.title }}
+    <div>
+      One-way data binding <br>
+      Two-way data binding <br>
+      v-model -> formulários
     </div>
+
+    <br> <br>
+
+    <input v-model="name" type="text">
+    
+    <br>
+
+    {{ name }}
+
+    <br> <br>
+
+    <div>
+      <label>Sports</label> <br>
+      <select v-model="sports">
+        <option value="">Escolha</option>
+        <option value="futebol">Futebol</option>
+        <option value="skate">Skate</option>
+        <option value="tenis">Tenis</option>
+      </select> <br>
+      {{ sports }}
+    </div>
+
+    <br> <br>
+
+    <div>
+      <label>Newsletter</label> <br>
+      <input v-model="newsletter" type="radio" value="Sim"> Sim
+      <input v-model="newsletter" type="radio" value="Não"> Não <br>
+      {{ newsletter }}
+    </div>
+
+    <br> <br>
+
+    <div>
+      <label>Contrato</label> <br>
+      <input v-model="contract" type="checkbox"> Aceita nossos termos... <br>
+      {{ contract }}
+    </div>
+
+    <br> <br>
+
+    <div>
+      <label>Cores que você mais gosta</label> <br>
+      <input v-model="colors" type="checkbox" value="Azul"> Azul <br>
+      <input v-model="colors" type="checkbox" value="Amarelo"> Amarelo <br>
+    </div>
+    {{ colors }}
   </div>
 </template>
 
@@ -34,44 +63,11 @@ export default {
   name: 'App', 
   data() {
     return {
-      isHome: true,
-      classVar: 'title',
-      styleClass: { color: 'aqua', backgroundColor: 'black', 'font-size': '20px' }, // Conseguimos fazer das duas formas
-      pClass: ['text', 'text-home'],
-      todos: [
-        {
-          "userId": 1,
-          "id": 1,
-          "title": "delectus aut autem",
-          "completed": false,
-          "imgSrc": 'https://placehold.co/150'
-        },
-        {
-          "userId": 1,
-          "id": 2,
-          "title": "quis ut nam facilis et officia qui",
-          "completed": false,
-          "imgSrc": 'https://placehold.co/150'
-        },
-        {
-          "userId": 1,
-          "id": 3,
-          "title": "fugiat veniam minus",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 4,
-          "title": "et porro tempora",
-          "completed": true
-        },
-        {
-          "userId": 1,
-          "id": 5,
-          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-          "completed": false
-        }
-      ]
+      name: 'Thiago Dupim',
+      sports: 'futebol',
+      newsletter: 'Não',
+      contract: false,
+      colors: []
     }
   }
 }
