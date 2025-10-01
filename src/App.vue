2e349f9feda315a88c45a65a3ee8446379845a44
Thiago Dupim Aguilar <thiagodupim3@gmail.com>
2025-10-01 17:11:31 -0300

    Observadores (watch)

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,22 @@
 <template>
   <div>
-    Computed properties <br>
-    {{ fullName }} <br>
-
-    <h2>Todos em aberto</h2>
-    <div v-for="todo in uncompletedTodos" :key="todo.id">
-      {{ todo.title }} 
-    </div>
-
-    <h2>Todos completas</h2>
-    <div v-for="todo in completedTodos" :key="todo.id">
-      {{ todo.title }} <span>{{ todo.completed }}</span>
-    </div>
+    <input v-model="name" type="text"> <br>
+    {{ name }}
 
     <br><br><br>
 
-    <h2>Todos</h2>
-    <div v-for="todo in todos" :key="todo.id">
-      <input v-model="todo.completed" type="checkbox">
-      {{ todo.title }}
-    </div>
+    <input v-model="user.first_name" type="text"> <br>
+    <input v-model="user.last_name" type="text"> <br>
+    {{ user.first_name }} {{ user.last_name }}
+
+    <br><br><br>
+
+    <select v-model="pageCount">
+      <option value="5">5</option>
+      <option value="10">10</option>
+      <option value="15">15</option>
+    </select> <br>
+    {{ pageCount }}
   </div>
 </template>
 
@@ -28,60 +25,44 @@ export default {
   name: 'App', 
   data() {
     return {
+      name: '',
+      pageCount: 5,
       user: {
-        first_name: 'Thiago',
-        last_name: 'Dupim',
-      },
-      todos: [
-        {
-          "userId": 1,
-          "id": 1,
-          "title": "delectus aut autem",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 2,
-          "title": "quis ut nam facilis et officia qui",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 3,
-          "title": "fugiat veniam minus",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 4,
-          "title": "et porro tempora",
-          "completed": true
-        },
-        {
-          "userId": 1,
-          "id": 5,
-          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-          "completed": false
-        }
-      ]
+        first_name: '',
+        last_name: '',
+      }
     }
   },
 
-  computed: {
-    fullName() {
-      return `${this.user.first_name} ${this.user.last_name}`
+  watch: {
+    name(value) {
+      if (value.length >= 3) {
+        this.saveUserName();
+      }
     },
-
-    uncompletedTodos() {
-      return this.todos.filter(todo => !todo.completed);
+    pageCount() {
+      this.changePage();
     },
-    completedTodos() {
-      return this.todos.filter(todo => todo.completed)
+    user: {
+      handler() { // Handler significa que quando o objeto alterar é para executa-lo
+        console.log('User alterado');
+      },
+      deep: true // Quando qualquer coisa dentro do o objeto se alterar dispare o código acima, então para observar o objeto profundamente precisamos transformar em deep
     },
   },
 
-  methods: {
+  computed: {
+    
+  },
 
+  methods: {
+    saveUserName() {
+      console.log('Ajax');
+      console.log(this.name);
+    },
+    changePage() {
+      console.log('Ajax changePage')
+    }
   }
 }
 </script>
