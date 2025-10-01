bdf0562595ba062a3e55318f9cee609533f97e89
Thiago Dupim Aguilar <thiagodupim3@gmail.com>
2025-10-01 10:53:15 -0300

    Propriedade computada (computed)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
-    <button @click="onClick">Enviar</button> <!-- Ao invés de usar v-on, podemos usar somente @-->
-  
-    <div @mouseover="onMouseOver" @mouseout="onMouseOut">
-      Mouse over
+    Computed properties <br>
+    {{ fullName }} <br>
+
+    <h2>Todos em aberto</h2>
+    <div v-for="todo in uncompletedTodos" :key="todo.id">
+      {{ todo.title }} 
     </div>
 
-    <br> <br>
+    <h2>Todos completas</h2>
+    <div v-for="todo in completedTodos" :key="todo.id">
+      {{ todo.title }} <span>{{ todo.completed }}</span>
+    </div>
 
-    <form action="https://google.com" @submit.prevent="onSubmit"> <!-- Modificadores- prevent para não prosseguir e abrir com o link do google -->
-      <input type="text" @keyup.enter="onKeyUp">
-      <button type="submit">Enviar</button>
-    </form>
+    <br><br><br>
+
+    <h2>Todos</h2>
+    <div v-for="todo in todos" :key="todo.id">
+      <input v-model="todo.completed" type="checkbox">
+      {{ todo.title }}
+    </div>
   </div>
 </template>
 
@@ -20,25 +28,60 @@ export default {
   name: 'App', 
   data() {
     return {
+      user: {
+        first_name: 'Thiago',
+        last_name: 'Dupim',
+      },
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        }
+      ]
     }
   },
 
+  computed: {
+    fullName() {
+      return `${this.user.first_name} ${this.user.last_name}`
+    },
+
+    uncompletedTodos() {
+      return this.todos.filter(todo => !todo.completed);
+    },
+    completedTodos() {
+      return this.todos.filter(todo => todo.completed)
+    },
+  },
+
   methods: {
-    onClick($evt) {
-      console.log('click', $evt);
-    },
-    onMouseOver($evt) {
-      console.log('mouse over', $evt);
-    },
-    onMouseOut($evt) {
-      console.log('mouse out', $evt);
-    },
-    onSubmit($evt) {
-      console.log('submit', $evt);
-    },
-    onKeyUp($evt) {
-      console.log('onKeyUp', $evt);
-    }
+
   }
 }
 </script>
